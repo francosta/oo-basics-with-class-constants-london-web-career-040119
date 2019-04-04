5de749a2229b9e46af61faaafe4a60c00a7f307c
Francisco Costa <francisco@fcosta.pt>
2019-04-04 17:59:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,12 +6,15 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(brand) != false
-      BRANDS << brand
-    end
   end
-  #   BRANDS << brand unless BRANDS.include?(brand)
+  
+  binding.pr
+  #   if BRANDS.include?(brand) != false
+  #     BRANDS << brand
+  #   end
   # end
+  # #   BRANDS << brand unless BRANDS.include?(brand)
+  # # end
 
   def cobble
     self.condition = "new"
